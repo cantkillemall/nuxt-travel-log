14cc168a0bb9c380c4faf47e125a5ec29a5e87df
Thierry Di Lenarda <cantkillemall@users.noreply.github.com>
2025-05-04 20:33:38 +0200

    feat: add landing page (#49)

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <h1 class="text-3xl font-bold underline">
-      Hello World!
-    </h1>
-    <button class="btn btn-secondary">
-      View map <Icon name="tabler:map" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
