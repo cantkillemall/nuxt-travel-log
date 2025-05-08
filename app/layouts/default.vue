2652233778508b937dd78e86eda01791bc913fb5
Cyqqut-nicnix-jodpo7 <thierry.dilenarda@gmail.com>
2025-05-08 12:35:41 +0200

    feat: initialize authentication store and enhance session handling during SSR

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,3 +1,10 @@
+<script lang="ts" setup>
+import { useAuthStore } from '~~/stores/auth';
+
+const authStore = useAuthStore();
+await authStore.init();
+</script>
+
 <template>
   <div class="flex min-h-screen flex-col">
     <AppNavBar />
