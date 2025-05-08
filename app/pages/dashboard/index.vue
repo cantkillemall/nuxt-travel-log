1dacda3274e5e457bba801caa5f8b298bede5c09
Thierry Di Lenarda <cantkillemall@users.noreply.github.com>
2025-05-08 19:01:55 +0200

    feat: add sidebar component and integrate it into the dashboard layout with collapsible animation and persistent state (#62)

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -1,5 +1,30 @@
+<script lang="ts" setup>
+const isSidebarOpen = ref(true);
+
+function toggleSidebar() {
+  isSidebarOpen.value = !isSidebarOpen.value;
+  localStorage.setItem('isSidebarOpen', isSidebarOpen.value.toString());
+}
+
+onMounted(() => {
+  isSidebarOpen.value = localStorage.getItem('isSidebarOpen') === 'true';
+});
+</script>
+
 <template>
-  <h1>
-    You are logged in!
-  </h1>
+  <div class="flex-1 flex">
+    <div class="bg-base-10 transition-all duration-300" :class="{ 'w-64': isSidebarOpen, 'w-16': !isSidebarOpen }">
+      <div class="flex hover:bg-base-200 hover:cursor-pointer mb-2 p-2" :class="{ 'justify-center': !isSidebarOpen, 'justify-end': isSidebarOpen }" @click="toggleSidebar">
+        <Icon v-if="isSidebarOpen" name="tabler:chevron-left" size="32" />
+        <Icon v-else name="tabler:chevron-right" size="32" />
+      </div>
+      <div class="flex flex-col gap-2">
+        <SidebarButton :show-label="isSidebarOpen" label="Locations" icon="tabler:map" href="/dashboard" />
+        <SidebarButton :show-label="isSidebarOpen" label="Add Location" icon="tabler:circle-plus-filled" href="/dashboard/add" />
+        <div class="divider" />
+        <SidebarButton :show-label="isSidebarOpen" label="Sign Out" icon="tabler:logout-2" href="/sign-out" />
+      </div>
+    </div>
+    <div class="flex-1" />
+  </div>
 </template>
