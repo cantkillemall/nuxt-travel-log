e931e6100bb75b3c7168f860dace6b0502ee857f
Cyqqut-nicnix-jodpo7 <thierry.dilenarda@gmail.com>
2025-05-10 17:24:56 +0200

    feat: Add MapLibre module, configures map SFC and styles according to color mode.

---
+++ app/components/App/map.client.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { CENTER_USA } from '~~/lib/db/constants';
+
+const colorMode = useColorMode();
+// const style = 'https://demotiles.maplibre.org/style.json';
+
+const style = computed(() => colorMode.value === 'dark' ? '/styles/dark.json' : 'https://tiles.openfreemap.org/styles/liberty');
+// const center = [3.33333, 50.48333];
+const zoom = 3;
+</script>
+
+<template>
+  <MglMap
+    :map-style="style"
+    :center="CENTER_USA"
+    :zoom="zoom"
+  >
+    <MglNavigationControl />
+  </MglMap>
+</template>
+
+<style>
+
+</style>
